2274648bc991b7cf4ba68960508250f1987273d2
receme <tahmid1989@gmail.com>
2016-05-28 03:12:45 +0600

    added to nuget package

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle ("SlidingLayerLib")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("Xamarin android porting for the library https://github.com/wunderlist/android-sliding-layer-lib")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
+[assembly: AssemblyCompany ("receme")]
 [assembly: AssemblyProduct ("")]
 [assembly: AssemblyCopyright ("receme")]
 [assembly: AssemblyTrademark ("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.0")]
+[assembly: AssemblyVersion ("1.0.4")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
